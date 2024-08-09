93ef78848530ff0aba62258570774e1953db51f7
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-08-09 15:08:11 -0600

    api connection commit

--- src/app/api/google/route.tsx
+++ src/app/api/google/route.tsx
@@ -7,10 +7,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     const   
- geocodingResponse = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyA2-uQU9Sav16a9BwifE1RL7WHZeOdlvM0`);
+ geocodingResponse = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=`);
     const coordinates = geocodingResponse.data.results[0].geometry.location;
 
-    const placesResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?location=${coordinates.lat},${coordinates.lng}&key=AIzaSyA2-uQU9Sav16a9BwifE1RL7WHZeOdlvM0`);
+    const placesResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?location=${coordinates.lat},${coordinates.lng}&key=`);
     const placeData = placesResponse.data.result;
 
     res.status(200).json({ coordinates, placeData });
