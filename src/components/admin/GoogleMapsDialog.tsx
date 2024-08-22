73d1c3adcaff41456a9350e76d461efae82e239b
Nestor Bonilla <nestor.bonilla.s@gmail.com>
2024-08-22 07:58:13 -0700

    testing with eip712 signatures

--- src/components/admin/GoogleMapsDialog.tsx
+++ src/components/admin/GoogleMapsDialog.tsx
@@ -8,6 +8,8 @@ import LocationSearch from "@/components/admin/LocationSearch";
 import { EAS, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
 import { useWallet } from "./WalletContext";
 import { ethers } from "ethers";
+import { SafeAccountV0_2_0 as SafeAccount, MetaTransaction } from "abstractionkit";
+import { sepolia, arbitrum } from "viem/chains";
 
 interface GoogleMapsDialogProps {
     onAddressSelect: (address: string) => void;
@@ -21,14 +23,54 @@ const GoogleMapsDialog: React.FC<GoogleMapsDialogProps> = ({ onAddressSelect, on
         smartAccount,
       } = useWallet();
     
-    const EAS_CONTRACT_ADDRESS = "0x4200000000000000000000000000000000000021";
-
+    // const EAS_CONTRACT_ADDRESS = "0xbD75f629A22Dc1ceD33dDA0b68c546A1c035c458"; // arbitrum one
+    const EAS_CONTRACT_ADDRESS = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; // ethereum sepolia
+    
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if (isAddressConfirmed) {
             onAddressSelect(selectedAddress);
             
-            const eas = new EAS(EAS_CONTRACT_ADDRESS);
+            // flow on candide wallet
+            const jsonRpcNodeProvider = "https://rpc2.sepolia.org";
+            const bundlerUrl = "https://sepolia.voltaire.candidewallet.com/rpc";
+
+            const transaction: MetaTransaction = {
+                to: EAS_CONTRACT_ADDRESS,
+                value: BigInt(0),
+                data: "0xf17325e7", // attest() => https://sepolia.etherscan.io/address/0xC2679fBD37d54388Ce493F1DB75320D236e1815e#writeContract#F1
+            }
+
+            let userOperation = await smartAccount!.createUserOperation(
+                [transaction],
+                jsonRpcNodeProvider,
+                bundlerUrl,
+            )
+
+            const domain = {
+                chainId: sepolia.id,
+                verifyingContract: smartAccount!.safe4337ModuleAddress,
+            };
+            const types = SafeAccount.EIP712_SAFE_OPERATION_TYPE;
+            
+            const { sender, ...userOp } = userOperation;
+            
+            const safeUserOperation = {
+                ...userOp,
+                safe: userOperation.sender,
+                validUntil: BigInt(0),
+                validAfter: BigInt(0),
+                entryPoint: smartAccount!.entrypointAddress,
+            };
+            
+            // const ownerPrivateKey = process.env.PRIVATE_KEY as string;
+            // const signer = new Wallet(ownerPrivateKey);
+            // const signature = await signer.signTypedData(domain, types, safeUserOperation);
+            // const formatedSig = SafeAccount.formatEip712SignaturesToUseroperationSignature([ownerPublicAddress], [signature]);
+            // userOperation.signature = formatedSig;
+
+            // flow on eas documenation
+            // const eas = new EAS(EAS_CONTRACT_ADDRESS);
             // eas.connect(signer!);
             // const schemaEncoder = new SchemaEncoder("string name, string physicalAddress, string score, string reviewCount");
             // const schemaUID = '0x390dae27a016f85da388c35e37a5ecba47ee2078ebff75ef36450d39e2d17409';
